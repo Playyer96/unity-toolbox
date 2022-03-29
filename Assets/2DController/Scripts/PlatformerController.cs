a7a928f1654ff0e2e58ba1e2868ec35a7234c051
Dani <dagugo.96@gmail.com>
2022-03-29 00:56:03 -0500

    [NEW] PlatformerControllerExtras Class setup

---
+++ Assets/2DController/Scripts/PlatformerController.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CubeS.Toolbox
+{
+    public class PlatformerController : MonoBehaviour
+    {
+        #region Properties
+        #endregion
+
+        #region Unity FUnctions
+        #endregion
+
+        #region Functions
+        #endregion
+    }
+}
